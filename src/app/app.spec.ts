a217721eac87b98d4fbbf466aec171ba9d857e4a
Mauricio Areas <Mauricio.Areas@capstonelogistics.com>
2025-12-10 09:37:34 -0600

    feat: add initial unit tests for the App component

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, curl-to-postman');
+    expect(compiled.querySelector('h1')?.textContent).toContain('cURL to Postman Converter');
   });
 });
